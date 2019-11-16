a6dd6cc303e49f96ce4a43197111b7760c25cabf
JA7XJ <josephmoscoso70@hotmail.com>
2019-11-16 17:05:17 -0600

    clase Nodo

---
+++ ProyectoEDD2/src/proyectoedd2/Nodo.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoedd2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author josep
+ */
+public class Nodo {
+
+    private ArrayList llaves = new ArrayList();
+    private ArrayList<Nodo> children = new ArrayList();
+    private int T;
+    private int t;
+    private boolean Leaf;
+
+    public Nodo(int orden) {
+        this.T = orden;//orden del arbol
+        this.t = (orden - 1) / 2;
+    }
+
+    public ArrayList getLlaves() {
+        return llaves;
+    }
+
+    public void setLlaves(ArrayList llaves) {
+        this.llaves = llaves;
+    }
+
+    public ArrayList<Nodo> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Nodo> children) {
+        this.children = children;
+    }
+
+    public int getT() {
+        return T;
+    }
+
+    public void setT(int T) {
+        this.T = T;
+    }
+
+    public int getTt() {
+        return t;
+    }
+
+    public void setTt(int t) {
+        this.t = t;
+    }
+
+    public boolean isLeaf() {
+        return Leaf;
+    }
+
+    public void setLeaf(boolean Leaf) {
+        this.Leaf = Leaf;
+    }
+
+    public boolean IsLeaf(boolean Leaf) {
+        if (this.Leaf == true) {//si es verdadero es Leaf
+            System.out.println("si es leaf");
+            return Leaf;
+        } else {//Si es falso no es Leaf
+            System.out.println("no es leaf");
+            return Leaf;
+        }
+    }
+}
