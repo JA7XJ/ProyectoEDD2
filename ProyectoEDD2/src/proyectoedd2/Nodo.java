eb1a86b29d5ee768abdbccfbe421e1725c0be318
mikeandino <henry@DESKTOP-4RCDP7N>
2019-12-01 13:17:31 -0600

    Bugfixs y Mas

--- ProyectoEDD2/src/proyectoedd2/Nodo.java
+++ ProyectoEDD2/src/proyectoedd2/Nodo.java
@@ -3,19 +3,26 @@ package proyectoedd2;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Nodo implements Serializable{
+public class Nodo implements Serializable {
 
     int nllaves = 0;
     ArrayList<Integer> llaves = new ArrayList<>();
     boolean hoja = true;
+    Nodo padre = null;
     ArrayList<Nodo> puntadores = new ArrayList<>();
 
     public Nodo(int orden) {
-        for (int i = 0; i < orden-1; i++) {
+        for (int i = 0; i < orden - 1; i++) {
             llaves.add(i, null);
         }
-        for (int i = 0; i <= orden-1; i++) {
+        for (int i = 0; i <= orden; i++) {
             puntadores.add(i, null);
+        }
+    }
+
+    public void setpadre() {
+        for (int i = 0; i < puntadores.size(); i++) {
+            puntadores.get(i).padre = this;
         }
     }
 }
